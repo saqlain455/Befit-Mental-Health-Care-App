c74d3990a72cfa4fff75e324653d3c9cd5bf8346
saqlaindev <70811314+saqlain455@users.noreply.github.com>
2021-12-07 01:37:31 +0500

    first commit

--- store/actions/videoActions.js
+++ store/actions/videoActions.js
@@ -1,28 +1,72 @@
 import  {
     ADD_STREAM,
     MY_STREAM,
-    ADD_REMOTE_STREAMSTREAM
+    ADD_REMOTE_STREAM
 } from './types'
 import IO from 'socket.io-client';
 import Peer from 'react-native-peerjs';
 
 // Api uri
-export const API_URI=`http://192.168.18.48:5000/`
+export const API_URI=`http://192.168.100.23:5000/`
 
 //socket config
-export const socket=IO("http://192.168.18.48:3000",{
+export const socket=IO("http://192.168.100.23:5000",{
     forceNew:true
 })
 
-socket.on('connection',()=>{
-    console.log('Connected client')
-})
 
 
-export const joinRoom=()=> async(dispatch)=>{
 
-}
+export const joinRoom=(stream)=> async(dispatch)=>{
+    const peerServer=new Peer(undefined,{
+        host:"192.168.100.23",
+        secure:false,
+        path:'/mypeer',
+        port:'5000'
+    })
 
-function connectToNewUser(){
+    dispatch({type:MY_STREAM,payload:stream});
+    
+    peerServer.on("error",(error)=>{
+        console.log('error',error)
+    })
+    
+    peerServer.on('open',(userId)=>{
+        const roomID="adsjfdlfjj111fdlfljffffffffhbjhdhdhjdlfs";
+        console.log(roomID,userId)
+        console.log("Open connection")
+         socket.emit('join-room',{userId,roomID})
+    
+    })
+
+    socket.on('connection',()=>{
+        console.log('Connected client')
+    })
+    
+ 
+
+    socket.on("user-connected",(userId)=>{
+        console.log("on user connected")
+        console.log(userId)
+        connectToNewUser(userId,stream)
+    })
+
+    peerServer.on('call',(call)=>{
+        call.answer(stream)
+        call.on('stream',(stream)=>{
+            dispatch({type:ADD_STREAM,payload:stream})
+        })
+    })
+
+    function connectToNewUser(userId,stream){
+        const call =peerServer.call(userId,stream)
+    
+        call.on('stream',(remoteStream)=>{
+            if(remoteStream){
+                dispatch({type:ADD_REMOTE_STREAM,payload:remoteStream})
+    
+            }
+        })
+    }
 
 }
--- videoCall.js
+++ videoCall.js
@@ -1,65 +1,141 @@
-import React,{useEffect} from "react";
-import {View} from 'react-native'
+import React, { useEffect } from "react";
+import { View, ScrollView, Text,Dimensions } from "react-native";
 import {
-    RTCPeerConnection,
-    RTCIceCandidate,
-    RTCSessionDescription,
-    RTCView,
-    MediaStream,
-    MediaStreamTrack,
-    mediaDevices,
-    registerGlobals
-  } from 'react-native-webrtc';
+  RTCPeerConnection,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCView,
+  MediaStream,
+  MediaStreamTrack,
+  mediaDevices,
+  registerGlobals,
+} from "react-native-webrtc";
 import { joinRoom } from "./store/actions/videoActions";
-import {connect} from 'react-redux'
- const VideoCall = () => {
-    useEffect(()=>{
+import { connect } from "react-redux";
 
-        let isFront = true;
+const {width, height} = Dimensions.get('window');
+const VideoCall = (props) => {
+  useEffect(() => {
+    let isFront = true;
 
-        mediaDevices.enumerateDevices().then((sourceInfos) => {
-          console.log(sourceInfos);
-          let videoSourceId;
-          for (let i = 0; i < sourceInfos.length; i++) {
-            const sourceInfo = sourceInfos[i];
-            if (
-              sourceInfo.kind == "videoinput" &&
-              sourceInfo.facing == (isFront ? "front" : "environment")
-            ) {
-              videoSourceId = sourceInfo.deviceId;
-            }
-          }
-          mediaDevices
-            .getUserMedia({
-              audio: true,
-              video: {
-                width: 640,
-                height: 480,
-                frameRate: 30,
-                facingMode: isFront ? "user" : "environment",
-                deviceId: videoSourceId,
-              },
-            })
-            .then((stream) => {
-              // Got stream!
-              console.log(stream)
-            })
-            .catch((error) => {
-              // Log error
-            });
+    mediaDevices.enumerateDevices().then((sourceInfos) => {
+      // console.log(sourceInfos);
+      let videoSourceId;
+      for (let i = 0; i < sourceInfos.length; i++) {
+        const sourceInfo = sourceInfos[i];
+        if (
+          sourceInfo.kind == "videoinput" &&
+          sourceInfo.facing == (isFront ? "front" : "environment")
+        ) {
+          videoSourceId = sourceInfo.deviceId;
+        }
+      }
+      mediaDevices
+        .getUserMedia({
+          audio: true,
+          video: {
+            width: 640,
+            height: 480,
+            frameRate: 30,
+            facingMode: isFront ? "user" : "environment",
+            deviceId: videoSourceId,
+          },
+        })
+        .then((stream) => {
+          // Got stream!
+          props.joinRoom(stream);
+          //   console.log(stream);
+        })
+        .catch((error) => {
+          // Log error
         });
-
-    },[])
-    return (
-        <View>
-            
+    });
+  }, []);
+  const { streams,remoteStreams } = props.video;
+    console.log("Other stream")
+    console.log(streams)
+    console.log(props.video.myStream)
+    console.log("end stream")
+  return (
+    <View style={{flex: 1, justifyContent: 'flex-start', padding: 10}}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: height * 0.5,
+            borderColor: 'yellow',
+            borderWidth: 4,
+          }}>
+          {props.video.myStream ? (
+            <RTCView
+              streamURL={props.video.myStream.toURL()}
+              style={{width, height: height * 0.4}}
+            />
+          ) : null}
         </View>
-    )
-}
+        <View style={{flex: 1, backgroundColor: 'black'}}>
+          <ScrollView horizontal style={{padding: 10}}>
+            {/* <>
+              {streams.length > 0 ? (
+                <>
+                  {streams.map((stream, index) => (
+                    <View
+                      key={index}
+                      style={{
+                        width: 280,
+                        backgroundColor: 'red',
+                        borderWidth: 1,
+                        borderColor: '#fff',
+                        marginRight: 10,
+                        padding: 5,
+                      }}>
+                      <RTCView
+                        streamURL={stream.toURL()}
+                        style={{width: 180, height: height * 0.4}}
+                      />
+                    </View>
+                  ))}
+                </>
+              ) : null}
+            </> */}
 
-const mapStateToProps=({video})=>({
-    video,
-})
+            <>
+              {remoteStreams ? (
+                remoteStreams.length > 0 ? (
+                  <>
+                    {remoteStreams.map((stream, index) => {
+                      return (
+                        <View
+                          key={index}
+                          style={{
+                            width: 280,
+                            backgroundColor: 'blue',
+                            borderWidth: 1,
+                            borderColor: '#fff',
+                            marginRight: 10,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                          }}>
+                          <RTCView
+                            streamURL={stream.toURL()}
+                            style={{width: 120, height: height * 0.4}}
+                          />
+                        </View>
+                      );
+                    })}
+                  </>
+                ) : null
+              ) : null}
+            </>
+          </ScrollView>
+        </View>
+      </View>
+  );
+};
 
+const mapStateToProps = ({ video }) => ({
+  video,
+});
 
-export default connect(mapStateToProps,{joinRoom})(VideoCall);
+export default connect(mapStateToProps, { joinRoom })(VideoCall);
